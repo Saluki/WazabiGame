f9efa232f01379fc74218d0f714a226d0ab5f26d
Saluki <saluki@gmx.fr>
2015-12-13 17:05:56 +0100

    Moving filters to annotations

--- WazabiBackend/src/ovh/gorillahack/wazabi/filters/ApiResponseFilter.java
+++ WazabiBackend/src/ovh/gorillahack/wazabi/filters/ApiResponseFilter.java
@@ -8,8 +8,10 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
 
+@WebFilter(urlPatterns="/api/*")
 public class ApiResponseFilter implements Filter {
 
 	protected FilterConfig config;
--- WazabiBackend/src/ovh/gorillahack/wazabi/filters/AuthFilter.java
+++ WazabiBackend/src/ovh/gorillahack/wazabi/filters/AuthFilter.java
@@ -8,7 +8,12 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 
+/**
+ * NOTE: ADDING API!!!
+ */
+@WebFilter(urlPatterns="/app/*")
 public class AuthFilter implements Filter {
 
 	protected FilterConfig config;
