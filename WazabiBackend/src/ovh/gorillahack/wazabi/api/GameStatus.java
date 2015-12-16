28657e5260edb958d70448840c37b43d71609969
Saluki <saluki@gmx.fr>
2015-12-17 00:31:55 +0100

    De-mocking partially /api/game/status
    Watch out, some features are still mocked or masked such as the player
    dices, the player cards, the skipped turns, ...

--- WazabiBackend/src/ovh/gorillahack/wazabi/api/GameStatus.java
+++ WazabiBackend/src/ovh/gorillahack/wazabi/api/GameStatus.java
@@ -1,9 +1,7 @@
 package ovh.gorillahack.wazabi.api;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.Stack;
-import java.util.Vector;
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -15,83 +13,131 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import ipl.mock.MockInterfaceGestionPartie;
+import ovh.gorillahack.wazabi.domaine.Joueur;
+import ovh.gorillahack.wazabi.domaine.Partie;
+import ovh.gorillahack.wazabi.exception.NoCurrentGameException;
+import ovh.gorillahack.wazabi.usecases.GestionPartie;
 
+@SuppressWarnings("unchecked")
 @WebServlet(urlPatterns = "/api/game/status")
 public class GameStatus extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	@EJB
-	private MockInterfaceGestionPartie gestionPartie;
-	// Developer notice
-	// [java.lang.NoClassDefFoundError: org/json/simple/JSONObject]
-	// => Use "Markers" > "Quick fix"
 
-	@SuppressWarnings("unchecked")
+	@EJB
+	private GestionPartie gestionPartie;
+
+	/**
+	 * L'objet JSON representant la reponse a renvoyer au client.
+	 */
+	protected JSONObject statusObject;
+
+	/**
+	 * Renvoie les informations de la en cours partie pour le joueur.
+	 * 
+	 * Les informations seront extraites pour le authentifie joueur se trouvant
+	 * dans la session et seront renvoyes selon les conventions REST au format
+	 * JSON.
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		JSONObject statusObject = new JSONObject();
+		statusObject = new JSONObject();
+		Joueur joueurSession = (Joueur) request.getSession().getAttribute("authenticated");
 
-		// information a resortir de la base de donn�e
-		Stack pile = gestionPartie.pileInformation();
-		// strucutre de la pile :
-		// 4)Vector<Map<String, Vector<String>>>
-		Vector<Map<String, Vector<String>>> challengerInfo = (Vector<Map<String, Vector<String>>>) pile.pop();
-		// 3)Vector<String>
-		Vector<String> desJoueurs = (Vector<String>) pile.pop();
-		// 2)Vector<Map<String, String>>
-		Vector<Map<String, String>> carteInfo = (Vector<Map<String, String>>) pile.pop();
-		// 1)Map<String, String>
-		Map<String, String> joueurInfo = (Map<String, String>) pile.pop();
+		try {
 
-		JSONObject playerInfo = new JSONObject();
-		playerInfo.putAll(joueurInfo);
+			addGameStatus();
+			addPlayerData(joueurSession);
+			addPlayerHand();
+			addChallengersData(joueurSession);
 
-		statusObject.put("player", playerInfo);
-
-		// Personal game data
-		JSONObject hand = new JSONObject();
-		JSONArray cards = new JSONArray();
-		for (int i = 0; i < carteInfo.size(); i++) {
-			// carte
-			JSONObject card = new JSONObject();
-			Map<String, String> mapCarte = carteInfo.get(i);
-			// ajout des cartes dans la cl� cards
-			card.putAll(mapCarte);
-			cards.add(card);
-
+		} catch (NoCurrentGameException e) {
+			response.setStatus(500);
+			return;
 		}
 
-		hand.put("cards", cards);
-
-		// rajout des d�s dans hand
-		JSONArray des = new JSONArray();
-		for (int i = 0; i < desJoueurs.size(); i++) {
-			des.add(desJoueurs.get(i));
-		}
-		hand.put("dices", des);
-		// rajoute de hand dans la strucuture principale
-		statusObject.put("hand", hand);
-
-		// Challengers game data
-
-		JSONArray challengers = new JSONArray();
-		
-		for (int i = 0; i < challengerInfo.size(); i++) {
-			JSONObject challenger = new JSONObject();
-			JSONArray desChallenger = new JSONArray();
-			Map<String, Vector<String>> mapChallenger = challengerInfo.get(i);
-			challenger.put("name", mapChallenger.get("name").get(0));
-			challenger.put("id", mapChallenger.get("id").get(0));
-			for (int j = 0; j < mapChallenger.get("dices").size(); j++) {
-				desChallenger.add(mapChallenger.get("dices").get(j));
-			}
-			challenger.put("dices", desChallenger);
-			
-			challengers.add(challenger);
-		}
-		statusObject.put("challengers", challengers);
 		response.getWriter().print(statusObject.toJSONString());
+	}
+
+	/**
+	 * Extrait l'etat du jeu pour le mettre dans la reponse JSON.
+	 * 
+	 * @throws NoCurrentGameException
+	 */
+	protected void addGameStatus() throws NoCurrentGameException {
+
+		Partie partieCourante = gestionPartie.getPartieCourante();
+		statusObject.put("status", partieCourante.getStatut().toString());
+	}
+
+	/**
+	 * Ajoute les informations de base du joueur dans la reponse JSON.
+	 * 
+	 * Extrait le pseudo ainsi que le nombre de tours a passer pour les ajouter
+	 * dans la reponse REST. Indique egalement a l'aide d'un booleen si c'est
+	 * actuellement le tour du joueur qui se trouve dans la session.
+	 * 
+	 * @param joueurSession
+	 *            Le joueur possedant la session
+	 */
+	protected void addPlayerData(Joueur joueurSession) {
+
+		Joueur joueurCourant = gestionPartie.getJoueurCourant();
+
+		JSONObject playerData = new JSONObject();
+		playerData.put("name", joueurSession.getPseudo());
+		playerData.put("play", joueurCourant.equals(joueurSession));
+		playerData.put("skip", 0); // TODO Add number skipped
+
+		statusObject.put("player", playerData);
+	}
+
+	/**
+	 * Ajoute les cartes et les des du joueur a la reponse JSON.
+	 * 
+	 * Selectionne toutes les informations concernant les cartes jeu et les
+	 * faces de des que le joueur possede dans sa "main".
+	 */
+	protected void addPlayerHand() {
+
+		JSONArray cardsObject = new JSONArray();
+		// TODO Get cards
+
+		JSONArray dicesArray = new JSONArray();
+		// TODO Get dices
+
+		JSONObject playerHandObject = new JSONObject();
+		playerHandObject.put("cards", cardsObject);
+		playerHandObject.put("dices", dicesArray);
+
+		statusObject.put("hand", playerHandObject);
+	}
+
+	/**
+	 * Ajoute les donnees concernant les adversaires du joueur.
+	 * 
+	 * Pour chaque challenger, son nom et sa combinaison de des, sont extraites
+	 * et mis dans l'objet JSON representant la reponse REST.
+	 * 
+	 * @param joueurSession
+	 *            Le joueur possedant la session
+	 * @throws NoCurrentGameException
+	 */
+	protected void addChallengersData(Joueur joueurSession) throws NoCurrentGameException {
+
+		JSONArray challengersList = new JSONArray();
+		List<Joueur> challengers = gestionPartie.getAdversaires(joueurSession);
+
+		for (Joueur challenger : challengers) {
+
+			JSONObject challengerObject = new JSONObject();
+			challengerObject.put("name", challenger.getPseudo());
+			challengerObject.put("dices", new JSONArray()); // TODO
+
+			challengersList.add(challengerObject);
+		}
+
+		statusObject.put("challengers", challengersList);
 	}
 }
