a90aae60cb29e73312668fed43b894711dc3985e
Alexandre Gielen <alexandre.gielen@student.vinci.be>
2015-12-17 00:12:02 +0100

    Ajout d'une exception dans le cas d'une pioche vide.

---
+++ WazabiEJB/ejbModule/ovh/gorillahack/wazabi/exception/CardStackEmptyException.java
@@ -0,0 +1,34 @@
+package ovh.gorillahack.wazabi.exception;
+
+public class CardStackEmptyException extends Exception {
+
+	public CardStackEmptyException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public CardStackEmptyException(Exception e) {
+		super(e);
+	}
+
+	public CardStackEmptyException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CardStackEmptyException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CardStackEmptyException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CardStackEmptyException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
