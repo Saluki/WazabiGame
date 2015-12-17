38d560c6fe1808223960e6dabc6855df6d0f4cbb
jvandera15 <jvandera15@LEN1436.stud.ipl.dom>
2015-12-17 14:30:54 +0100

    creation du mainTestDeconnection

---
+++ WazabiEJB_mainTest/src/MainTestDeconnection.java
@@ -0,0 +1,43 @@
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import ovh.gorillahack.wazabi.domaine.Carte;
+import ovh.gorillahack.wazabi.domaine.Joueur;
+import ovh.gorillahack.wazabi.domaine.Partie;
+import ovh.gorillahack.wazabi.domaine.Partie.Status;
+import ovh.gorillahack.wazabi.exception.NoCurrentGameException;
+import ovh.gorillahack.wazabi.exception.ValidationException;
+import ovh.gorillahack.wazabi.exception.XmlParsingException;
+import ovh.gorillahack.wazabi.usecases.GestionPartie;
+
+public class MainTestDeconnection {
+	public static void main(String[] args) throws ValidationException, NoCurrentGameException, XmlParsingException {
+		try {
+			Context jndi = new InitialContext();
+			GestionPartie gestionPartie = (GestionPartie) jndi
+					.lookup("ejb:Wazabi/WazabiEJB/GestionPartieImpl!ovh.gorillahack.wazabi.usecases.GestionPartie");
+			Joueur joueur1 = gestionPartie.inscrire("test1", "test1", "test1");
+			Joueur joueur2 = gestionPartie.inscrire("test2", "test2", "test2");
+			Joueur joueur3 = gestionPartie.inscrire("test3", "test3", "test3");
+
+
+			Partie partie1 = gestionPartie.creerPartie("HELLO");
+			System.out.println(partie1.getNom());
+			gestionPartie.rejoindrePartie(joueur1);
+			gestionPartie.rejoindrePartie(joueur1);
+			gestionPartie.rejoindrePartie(joueur3);
+			if(gestionPartie.getPartieCourante().getStatut()==Status.COMMENCE)
+				System.out.println("La partie commence!");
+			System.out.println("Le joueur " + gestionPartie.getJoueurCourant().getPseudo() + " commence");
+			gestionPartie.deconnecter(joueur1);
+			gestionPartie.deconnecter(joueur2);
+			System.out.println("Le joueur "+ gestionPartie.getPartieCourante().getVainqueur().toString() +" gagne la partie");
+			
+		} catch (NamingException exception) {
+			exception.printStackTrace();
+		}
+	}
+}
